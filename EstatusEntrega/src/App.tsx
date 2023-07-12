0e8d5333ef9c7dfe0ccb7da5d4280f3239b547a8
AngelFmc <angelfmc01@hotmail.com>
2023-07-12 14:45:04 -0600

    Second Change Styles
    Se hicieron segundos cambios al diseño

--- EstatusEntrega/src/App.tsx
+++ EstatusEntrega/src/App.tsx
@@ -4,165 +4,149 @@ function App() {
   return (
     <>
       <div className="w-full items-center">
-        <div className="w-[390px] flex flex-row bg-blue-100 ml-[17.3%] mt-4 border-[2px] border-gray-50">
+        <div className="w-[390px] flex flex-row bg-blue-100 ml-[17.2%] mt-4 border-[1px] border-gray-50">
           <h2 className="mr-4 text-center px-10 border-r-2 border-gray-50">
             ESTATUS SEM ENTREGA{" "}
           </h2>
           <h2 className="text-center px-10">Varios elementos 29</h2>
         </div>
 
-        <table className="m-auto mt-5 border-[2px] border-gray-200 ">
-          <thead className="border-b-[2px] border-gray-200">
+        <table className="m-auto mt-5 border-[1px] border-gray-200 ">
+          <thead className="border-b-[1px] border-gray-200 bg-blue-100">
             <tr>
-              <th className="px-16 bg-blue-100 border-r-[2px] border-gray-50">
+              <th className="px-16 border-r-[1px] border-gray-50">
                 Suma de Cajas PEDIDOS HF
               </th>
-              <th className="px-20 bg-blue-100 border-r-[2px] border-gray-50">
+              <th className="px-20 border-r-[1px] border-gray-50">
                 Etiquetas de columna 14-jul
               </th>
-              <th className="p-2 bg-blue-100 border-r-[2px] border-gray-50">
-                15-jul
-              </th>
-              <th className="p-2 bg-blue-100 border-r-[2px] border-gray-50">
-                17-jul
-              </th>
-              <th className="p-2 bg-blue-100 border-r-[2px] border-gray-50">
-                18-jul
-              </th>
-              <th className="p-2 bg-blue-100">19-jul</th>
+              <th className="p-2 border-r-[1px] border-gray-50">15-jul</th>
+              <th className="p-2 border-r-[1px] border-gray-50">17-jul</th>
+              <th className="p-2 border-r-[1px] border-gray-50">18-jul</th>
+              <th className="p-2">19-jul</th>
               <th className="p-2 ">TOTAL</th>
             </tr>
           </thead>
 
           <tbody>
-            <tr className="text-center border-b-[2px] border-gray-50">
-              <td className="border-r-[2px] border-gray-50">
+            <tr className="text-center border-b-[1px] border-gray-50">
+              <td className="border-r-[1px] border-gray-50">
                 Slice Super Select 72cts
               </td>
-              <td className="border-r-[2px] border-gray-50"></td>
-              <td className="border-r-[2px] border-gray-50">28</td>
-              <td className="border-r-[2px] border-gray-50">42</td>
-              <td className="border-r-[2px] border-gray-50">28</td>
-              <td className="border-r-[2px] border-gray-50"></td>
-              <th>98</th>
+              <td className="border-r-[1px] border-gray-50"></td>
+              <td className="border-r-[1px] border-gray-50">28</td>
+              <td className="border-r-[1px] border-gray-50">42</td>
+              <td className="border-r-[1px] border-gray-50">28</td>
+              <td className="border-r-[1px] border-gray-50"></td>
+              <td>98</td>
             </tr>
 
-            <tr className="text-center border-b-[2px] border-gray-50">
-              <td className="border-r-[2px] border-gray-50">Bells cv L</td>
-              <td className="border-r-[2px] border-gray-50">326</td>
-              <td className="border-r-[2px] border-gray-50">157</td>
-              <td className="border-r-[2px] border-gray-50">771</td>
-              <td className="border-r-[2px] border-gray-50">207</td>
-              <td className="border-r-[2px] border-gray-50">210</td>
-              <th className="border-r-[2px] border-gray-50">1671</th>
+            <tr className="text-center border-b-[1px] border-gray-50">
+              <td className="border-r-[1px] border-gray-50">Bells cv L</td>
+              <td className="border-r-[1px] border-gray-50">326</td>
+              <td className="border-r-[1px] border-gray-50">157</td>
+              <td className="border-r-[1px] border-gray-50">771</td>
+              <td className="border-r-[1px] border-gray-50">207</td>
+              <td className="border-r-[1px] border-gray-50">210</td>
+              <td>1671</td>
             </tr>
 
-            <tr className="text-center border-b-[2px] border-gray-50">
-              <td className="border-r-[2px] border-gray-50">Grape cv</td>
-              <td className="border-r-[2px] border-gray-50"></td>
-              <td className="border-r-[2px] border-gray-50">162</td>
-              <td className="border-r-[2px] border-gray-50"></td>
-              <td className="border-r-[2px] border-gray-50">162</td>
-              <td className="border-r-[2px] border-gray-50"></td>
-              <th>324</th>
+            <tr className="text-center border-b-[1px] border-gray-50">
+              <td className="border-r-[1px] border-gray-50">Grape cv</td>
+              <td className="border-r-[1px] border-gray-50"></td>
+              <td className="border-r-[1px] border-gray-50">162</td>
+              <td className="border-r-[1px] border-gray-50"></td>
+              <td className="border-r-[1px] border-gray-50">162</td>
+              <td className="border-r-[1px] border-gray-50"></td>
+              <td>324</td>
             </tr>
 
-            <tr className="text-center border-b-[2px] border-gray-50">
-              <td className="border-r-[2px] border-gray-50">Persa CV 20 lbs</td>
-              <td className="border-r-[2px] border-gray-50">7</td>
-              <td className="border-r-[2px] border-gray-50">52</td>
-              <td className="border-r-[2px] border-gray-50">97</td>
-              <td className="border-r-[2px] border-gray-50">52</td>
-              <td className="border-r-[2px] border-gray-50">40</td>
-              <th>248</th>
+            <tr className="text-center border-b-[1px] border-gray-50">
+              <td className="border-r-[1px] border-gray-50">Persa CV 20 lbs</td>
+              <td className="border-r-[1px] border-gray-50">7</td>
+              <td className="border-r-[1px] border-gray-50">52</td>
+              <td className="border-r-[1px] border-gray-50">97</td>
+              <td className="border-r-[1px] border-gray-50">52</td>
+              <td className="border-r-[1px] border-gray-50">40</td>
+              <td>248</td>
             </tr>
 
-            <tr className="text-center border-b-[2px] border-gray-50">
-              <td className="border-r-[2px] border-gray-50">Roma L</td>
-              <td className="border-r-[2px] border-gray-50">233</td>
-              <td className="border-r-[2px] border-gray-50">147</td>
-              <td className="border-r-[2px] border-gray-50">233</td>
-              <td className="border-r-[2px] border-gray-50">147</td>
-              <td className="border-r-[2px] border-gray-50"></td>
-              <th>760</th>
+            <tr className="text-center border-b-[1px] border-gray-50">
+              <td className="border-r-[1px] border-gray-50">Roma L</td>
+              <td className="border-r-[1px] border-gray-50">233</td>
+              <td className="border-r-[1px] border-gray-50">147</td>
+              <td className="border-r-[1px] border-gray-50">233</td>
+              <td className="border-r-[1px] border-gray-50">147</td>
+              <td className="border-r-[1px] border-gray-50"></td>
+              <td>760</td>
             </tr>
 
-            <tr className="text-center border-b-[2px] border-gray-50">
-              <td className="bg-green-600 border-r-[2px] border-gray-50">
+            <tr className="text-center border-b-[1px] border-gray-50">
+              <td className=" border-r-[1px] border-gray-50">
                 Org bell 11 lbs
               </td>
-              <td className="bg-green-600 border-r-[2px] border-gray-50">
-                291
-              </td>
-              <td className="bg-green-600 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-600 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-600 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-600 border-r-[2px] border-gray-50"></td>
-              <th>291</th>
+              <td className=" border-r-[1px] border-gray-50">291</td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td>291</td>
             </tr>
 
-            <tr className="text-center border-b-[2px] border-gray-50">
-              <td className="bg-green-700 border-r-[2px] border-gray-50">
+            <tr className="text-center border-b-[1px] border-gray-50">
+              <td className=" border-r-[1px] border-gray-50">
                 Org bell 25 lbs
               </td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50">
-                182
-              </td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <th>182</th>
+              <td className=" border-r-[1px] border-gray-50">182</td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td>182</td>
             </tr>
 
-            <tr className="text-center border-b-[2px] border-gray-50">
-              <td className="bg-green-700 border-r-[2px] border-gray-50">
-                Roma org xl
-              </td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50">
-                175
-              </td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <th>175</th>
+            <tr className="text-center border-b-[1px] border-gray-50">
+              <td className=" border-r-[1px] border-gray-50">Roma org xl</td>
+              <td className=" border-r-[1px] border-gray-50">175</td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td>175</td>
             </tr>
 
-            <tr className="text-center border-b-[2px] border-gray-50">
-              <td className="bg-green-700 border-r-[2px] border-gray-50">
-                Grape org
-              </td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50">21</td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <th>21</th>
+            <tr className="text-center border-b-[1px] border-gray-50">
+              <td className=" border-r-[1px] border-gray-50">Grape org</td>
+              <td className=" border-r-[1px] border-gray-50">21</td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td>21</td>
             </tr>
 
-            <tr className="text-center border-b-[2px] border-gray-50">
-              <td className="bg-green-700 border-r-[2px] border-gray-50">
+            <tr className="text-center border-b-[1px] border-gray-50">
+              <td className=" border-r-[1px] border-gray-50">
                 Green bell org #1
               </td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50">16</td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <th>16</th>
+              <td className=" border-r-[1px] border-gray-50">16</td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td>16</td>
             </tr>
 
-            <tr className="text-center border-b-[2px] border-gray-50">
-              <td className="bg-green-700 border-r-[2px] border-gray-50">
+            <tr className="text-center border-b-[1px] border-gray-50">
+              <td className=" border-r-[1px] border-gray-50">
                 Green bell org #2
               </td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50">25</td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <td className="bg-green-700 border-r-[2px] border-gray-50"></td>
-              <th>25</th>
+              <td className=" border-r-[1px] border-gray-50">25</td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td className=" border-r-[1px] border-gray-50"></td>
+              <td>25</td>
             </tr>
           </tbody>
 
